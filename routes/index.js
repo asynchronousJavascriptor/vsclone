89e169c1432953f9b1fdc09238c7338f61beb709
Harsh Vandana Sharma <harshu8545@gmail.com>
2023-07-21 10:24:45 +0530

    third change

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,22 @@ var router = express.Router();
 const fs = require("fs");
 
 router.get('/', function(req, res) {
-  res.render("index");
+  fs.readdir(`./uploads`, {withFileTypes: true}, function(err, files){
+    res.render("index", {files})
+  })
 });
 
 router.get('/createfile', function(req, res) {
   fs.writeFile(`./uploads/${req.query.filename}`, "", function(err){
     if(err) res.send(err);
     else res.redirect("back")
+  })
+});
+
+router.get('/createfolder', function(req, res) {
+  fs.mkdir(`./uploads/${req.query.foldername}`, function(err){
+    if(err) return err;
+    else res.redirect("back");    
   })
 });
 
